8f2f0b8436437b77e9f25b363de89476bff32b6f
Parthib Roy <proy@lbl.gov>
2024-08-10 19:16:21 -0700

    Added some more IDs to UI components

--- src/python/impactx/dashboard/__main__.py
+++ src/python/impactx/dashboard/__main__.py
@@ -62,7 +62,7 @@ with RouterViewLayout(server, "/Analyze"):
 # GUI
 # -----------------------------------------------------------------------------
 def init_terminal():
-    with xterm.XTerm(v_if="$route.path == '/Run'") as term:
+    with xterm.XTerm(v_if="$route.path == '/Run'", id="xterm_component") as term:
         ctrl.terminal_print = term.writeln
 
 

1a1bc7e9cbea1cc1a202c5474f3e473fc293877e
Parthib Roy <proy@lbl.gov>
2025-02-20 19:13:36 -0800

    Handle selection of collective effects based on tracking_mode selection

--- src/python/impactx/dashboard/Input/inputParameters/inputMain.py
+++ src/python/impactx/dashboard/Input/inputParameters/inputMain.py
@@ -28,6 +28,18 @@ class InputParameters(CardBase):
         if state.kin_energy_on_ui != 0:
             InputFunctions.update_kin_energy_sim_value()
 
+    @state.change("tracking_mode")
+    def on_tracking_mode_change(tracking_mode, **kwargs) -> None:
+        if tracking_mode in ("Envelope Tracking", "Reference Tracking"):
+            state.space_charge = False
+            state.csr = False
+
+            state.disable_space_charge = True
+            state.disable_csr = True
+        else:
+            state.disable_space_charge = False
+            state.disable_csr = False
+
     def card_content(self):
         with vuetify.VCard(style=self.collapsable):
             CardComponents.input_header(self.HEADER_NAME)
@@ -42,12 +54,14 @@ class InputParameters(CardBase):
                             label="Space Charge",
                             v_model=("space_charge", False),
                             dense=True,
+                            disabled=("disable_space_charge",),
                         )
                     with vuetify.VCol(cols="auto"):
                         vuetify.VCheckbox(
                             label="CSR",
                             v_model=("csr", False),
                             dense=True,
+                            disabled=("disable_csr",),
                         )
                 with vuetify.VRow(**self.ROW_STYLE):
                     with vuetify.VCol(cols=6):

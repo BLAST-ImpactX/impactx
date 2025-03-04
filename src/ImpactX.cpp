ee347b2650271b00e1f9b740f73dbc6686608ce1
Axel Huebl <axel.huebl@plasma.ninja>
2025-03-04 11:54:23 -0800

    Write `impactx_used_inputs`
    Similar to WarpX, write out the values of `amrex::ParmParse` that
    we use to initialize/control the simulation.

--- src/ImpactX.cpp
+++ src/ImpactX.cpp
@@ -14,6 +14,8 @@
 #include "diagnostics/DiagnosticOutput.H"
 #include "particles/wakefields/HandleWakefield.H"
 
+#include <ablastr/utils/UsedInputsFile.H>
+
 #include <AMReX.H>
 #include <AMReX_AmrParGDB.H>
 #include <AMReX_BLProfiler.H>
@@ -116,6 +118,15 @@ namespace impactx {
                 std::cout << "\nGrids Summary:\n";
                 amr_data->printGridSummary(std::cout, 0, amr_data->finestLevel());
             }
+        }
+
+        // write parameters
+        {
+            std::string filename = "impactx_used_inputs";
+            amrex::ParmParse pp_impactx("impactx");
+            pp_impactx.queryAdd("used_inputs_file", filename);
+
+            ablastr::utils::write_used_inputs_file(filename);
         }
 
         // keep track that init is done

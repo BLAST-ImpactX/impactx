23408d98a019cc3999ecec11f3d00b02d591a131
Parthib Roy <proy@lbl.gov>
2025-03-29 10:04:40 -0700

    add: copyright headers

--- src/python/impactx/dashboard/Toolbar/sim_history/helper.py
+++ src/python/impactx/dashboard/Toolbar/sim_history/helper.py
@@ -1,3 +1,11 @@
+"""
+This file is part of ImpactX
+
+Copyright 2025 ImpactX contributors
+Authors: Parthib Roy
+License: BSD-3-Clause-LBNL
+"""
+
 from ... import html, setup_server, vuetify
 
 server, state, ctrl = setup_server()

040065992443132b893ad1afba1f83f242696697
cemitch99 <46825199+cemitch99@users.noreply.github.com>
2025-04-21 19:27:17 +0000

    Update Stub Files

--- src/python/impactx/impactx_pybind/__init__.pyi
+++ src/python/impactx/impactx_pybind/__init__.pyi
@@ -775,6 +775,6 @@ __author__: str = (
     "Axel Huebl, Chad Mitchell, Ryan Sandberg, Marco Garten, Ji Qiang, et al."
 )
 __license__: str = "BSD-3-Clause-LBNL"
-__version__: str = "25.03"
+__version__: str = "25.04"
 s: CoordSystem  # value = <CoordSystem.s: 0>
 t: CoordSystem  # value = <CoordSystem.t: 1>

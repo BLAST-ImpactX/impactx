5f253444fb9ed4a5b4bf23127cfe6041a8568a32
Parthib Roy <proy@lbl.gov>
2025-04-22 20:19:36 -0700

    add: handle a failed sim in 'sim_history'
    Users can view a failed sim in sim history now

--- src/python/impactx/dashboard/Toolbar/sim_history/custom.js
+++ src/python/impactx/dashboard/Toolbar/sim_history/custom.js
@@ -1,6 +1,7 @@
 window.getSimStatusColor = function(status) {
     if (status === 'Completed') return 'success';
     if (status === 'In Progress') return 'info';
+    if (status === 'Failed') return 'error';
     return 'default';
 }
 

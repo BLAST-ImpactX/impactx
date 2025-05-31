a7cc94041300282246ecccb139d95138c167ee7e
Parthib Roy <proy@lbl.gov>
2025-05-31 11:21:14 -0700

    break parse_lattice_elements into smaller helper functions

--- src/python/impactx/dashboard/Toolbar/file_imports/python/lattice_helper.py
+++ src/python/impactx/dashboard/Toolbar/file_imports/python/lattice_helper.py
@@ -1,4 +1,3 @@
-
 import re
 
 class DashboardLatticeConfigParser:
@@ -6,14 +5,20 @@ class DashboardLatticeConfigParser:
     @staticmethod
     def parse_lattice_elements(content: str) -> dict:
         """
-        Parses lattice elements from the simulation file content.
+        Parses lattice elements and also extracts used lattice variables.
+        """
+        result = DashboardLatticeConfigParser.parse_simple_lattice(content)
+        result["used_lattice_variables"] = DashboardLatticeConfigParser.extract_used_variables(result)
 
-        :param content: The content of the ImpactX simulation file.
+        return result
+
+    @staticmethod
+    def parse_simple_lattice(content: str) -> dict:
+        """
+        Parses simple lattice elements from the simulation file content.
         """
 
         dictionary = {"lattice_elements": []}
-        used_variables = set()
-
         lattice_elements = re.findall(r"elements\.(\w+)\((.*?)\)", content)
 
         for element_name, element_parameter in lattice_elements:
@@ -23,9 +28,20 @@ class DashboardLatticeConfigParser:
             for parameter_name, parameter_value in parameter_pairs:
                 parameter_value_cleaned = parameter_value.strip("'\"")
                 element["parameters"][parameter_name] = parameter_value_cleaned
-                used_variables.add(parameter_value_cleaned)
 
             dictionary["lattice_elements"].append(element)
 
-        dictionary["used_lattice_variables"] = used_variables
         return dictionary
+
+    @staticmethod
+    def extract_used_variables(parsed_lattice: dict) -> set:
+        """
+        Extracts used lattice variables from parsed lattice data.
+        """
+        used_variables = set()
+
+        for element in parsed_lattice["lattice_elements"]:
+            for value in element["parameters"].values():
+                used_variables.add(value)
+
+        return used_variables

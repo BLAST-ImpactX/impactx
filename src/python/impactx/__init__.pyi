f050c33c7e3419ab9692da71e3cd6b7d26dce9f5
ax3l <1353258+ax3l@users.noreply.github.com>
2025-07-23 16:24:22 +0000

    Update Stub Files

--- src/python/impactx/__init__.pyi
+++ src/python/impactx/__init__.pyi
@@ -86,7 +86,7 @@ __author__: str = (
     "Axel Huebl, Chad Mitchell, Ryan Sandberg, Marco Garten, Ji Qiang, et al."
 )
 __license__: str = "BSD-3-Clause-LBNL"
-__version__: str = "25.06"
+__version__: str = "25.07"
 s: impactx_pybind.CoordSystem  # value = <CoordSystem.s: 0>
 t: impactx_pybind.CoordSystem  # value = <CoordSystem.t: 1>
 cxx = impactx_pybind

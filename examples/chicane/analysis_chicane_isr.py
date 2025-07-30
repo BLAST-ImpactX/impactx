3066eb817dc29ae414df966cbe68102c38a0e406
Axel Huebl <axel.huebl@plasma.ninja>
2025-07-30 12:21:42 -0700

    Update examples/chicane/analysis_chicane_isr.py

--- examples/chicane/analysis_chicane_isr.py
+++ examples/chicane/analysis_chicane_isr.py
@@ -79,7 +79,7 @@ print(
 )
 
 atol = 0.0  # ignored
-rtol = 2.3 * num_particles**-0.5  # from random sampling of a smooth distribution
+rtol = 2.4 * num_particles**-0.5  # from random sampling of a smooth distribution
 print(f"  rtol={rtol} (ignored: atol~={atol})")
 
 assert np.allclose(

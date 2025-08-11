9e50c8c7ca5a415759aa7b1f4db86d857d9f865f
Axel Huebl <axel.huebl@plasma.ninja>
2025-08-11 16:25:42 -0700

    Pull (py)AMReX/WarpX 25.08
    For FASTMATH options

--- cmake/dependencies/ABLASTR.cmake
+++ cmake/dependencies/ABLASTR.cmake
@@ -175,7 +175,7 @@ set(ImpactX_openpmd_src ""
 set(ImpactX_ablastr_repo "https://github.com/BLAST-WarpX/warpx.git"
     CACHE STRING
     "Repository URI to pull and build ABLASTR from if(ImpactX_ablastr_internal)")
-set(ImpactX_ablastr_branch "06feda8131de7b97f2cc16a6e244d1d4f9874c3b"
+set(ImpactX_ablastr_branch "25.08"
     CACHE STRING
     "Repository branch for ImpactX_ablastr_repo if(ImpactX_ablastr_internal)")
 

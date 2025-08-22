eac18d464461705270c72494e28db5c9e3f9502b
Kyrre Ness Sjobak <k.n.sjobak@fys.uio.no>
2025-08-22 14:55:53 +0200

    Refactoring

--- examples/active_plasma_lens/analysis_APL_ChrPlasmaLens_zero.py
+++ examples/active_plasma_lens/analysis_APL_ChrPlasmaLens_zero.py
@@ -4,42 +4,12 @@
 # Authors: Axel Huebl, Chad Mitchell, Kyrre Sjobak
 # License: BSD-3-Clause-LBNL
 #
+# -*- coding: utf-8 -*-
 
-
-import numpy as np
-import openpmd_api as io
-from scipy.stats import moment
-
-
-def get_moments(beam):
-    """Calculate standard deviations of beam position & momenta
-    and emittance values
-
-    Returns
-    -------
-    sigx, sigy, sigt, emittance_x, emittance_y, emittance_t
-    """
-    sigx = moment(beam["position_x"], moment=2) ** 0.5  # variance -> std dev.
-    sigpx = moment(beam["momentum_x"], moment=2) ** 0.5
-    sigy = moment(beam["position_y"], moment=2) ** 0.5
-    sigpy = moment(beam["momentum_y"], moment=2) ** 0.5
-    sigt = moment(beam["position_t"], moment=2) ** 0.5
-    sigpt = moment(beam["momentum_t"], moment=2) ** 0.5
-
-    epstrms = beam.cov(ddof=0)
-    emittance_x = (sigx**2 * sigpx**2 - epstrms["position_x"]["momentum_x"] ** 2) ** 0.5
-    emittance_y = (sigy**2 * sigpy**2 - epstrms["position_y"]["momentum_y"] ** 2) ** 0.5
-    emittance_t = (sigt**2 * sigpt**2 - epstrms["position_t"]["momentum_t"] ** 2) ** 0.5
-
-    return (sigx, sigy, sigt, emittance_x, emittance_y, emittance_t)
-
+from analysis_APL_ChrPlasmaLens import *
 
 # initial/final beam
-series = io.Series("diags/openPMD/monitor.h5", io.Access.read_only)
-last_step = list(series.iterations)[-1]
-initial = series.iterations[1].particles["beam"].to_df()
-beam_final = series.iterations[last_step].particles["beam"]
-final = beam_final.to_df()
+(initial,beam_final,final) = getBeams()
 
 # compare number of particles
 num_particles = 10000

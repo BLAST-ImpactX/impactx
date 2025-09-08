615413252c54c296b177d521c3647c0ffc4d81de
Chad Mitchell <46825199+cemitch99@users.noreply.github.com>
2025-09-08 10:35:20 -0700

    Apply suggestions from code review
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>

--- examples/solenoid_softedge/run_solenoid_softedge_solvable.py
+++ examples/solenoid_softedge/run_solenoid_softedge_solvable.py
@@ -24,7 +24,6 @@ sim.init_grids()
 # initial vertical rms emittance of 2 um
 kin_energy_MeV = 250.0  # reference energy
 bunch_charge_C = 1.0e-9  # used with space charge
-npart = 10000  # number of macro particles
 
 #   reference particle
 ref = sim.particle_container().ref_particle()
@@ -69,9 +68,10 @@ for p_dpy in dpy:
 for p_dpt in dpt:
     dpt_podv.push_back(p_dpt)
 
-pc.add_n_particles(
-    dx_podv, dy_podv, dt_podv, dpx_podv, dpy_podv, dpt_podv, qm_eev, bunch_charge_C
-)
+if amr.ParallelDescriptor.IOProcessor():
+    pc.add_n_particles(
+        dx_podv, dy_podv, dt_podv, dpx_podv, dpy_podv, dpt_podv, qm_eev, bunch_charge_C
+    )
 
 # design the accelerator lattice
 sol = elements.SoftSolenoid(

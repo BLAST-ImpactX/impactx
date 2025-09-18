75d25a98ca15d7dfe1440cbeeea25baca0640651
Axel Huebl <axel.huebl@plasma.ninja>
2025-09-18 14:08:55 -0700

    Update analysis

--- examples/fodo_space_charge/analysis_fodo_Gauss3D_sc.py
+++ examples/fodo_space_charge/analysis_fodo_Gauss3D_sc.py
@@ -5,85 +5,60 @@
 # License: BSD-3-Clause-LBNL
 #
 
-import glob
-import re
-
 import numpy as np
-import pandas as pd
+import openpmd_api as io
+from scipy.stats import moment
 
 
-def read_file(file_pattern):
-    for filename in glob.glob(file_pattern):
-        df = pd.read_csv(filename, delimiter=r"\s+")
-        if "step" not in df.columns:
-            step = int(re.findall(r"[0-9]+", filename)[0])
-            df["step"] = step
-        yield df
-
-
-def read_time_series(file_pattern):
-    """Read in all CSV files from each MPI rank (and potentially OpenMP
-    thread). Concatenate into one Pandas dataframe.
+def get_moments(beam):
+    """Calculate standard deviations of beam position & momenta
+    and emittance values
 
     Returns
     -------
-    pandas.DataFrame
+    sigx, sigy, sigt, emittance_x, emittance_y, emittance_t
     """
-    return pd.concat(
-        read_file(file_pattern),
-        axis=0,
-        ignore_index=True,
-    )  # .set_index('id')
+    sigx = moment(beam["position_x"], moment=2) ** 0.5  # variance -> std dev.
+    sigpx = moment(beam["momentum_x"], moment=2) ** 0.5
+    sigy = moment(beam["position_y"], moment=2) ** 0.5
+    sigpy = moment(beam["momentum_y"], moment=2) ** 0.5
+    sigt = moment(beam["position_t"], moment=2) ** 0.5
+    sigpt = moment(beam["momentum_t"], moment=2) ** 0.5
+
+    epstrms = beam.cov(ddof=0)
+    emittance_x = (sigx**2 * sigpx**2 - epstrms["position_x"]["momentum_x"] ** 2) ** 0.5
+    emittance_y = (sigy**2 * sigpy**2 - epstrms["position_y"]["momentum_y"] ** 2) ** 0.5
+    emittance_t = (sigt**2 * sigpt**2 - epstrms["position_t"]["momentum_t"] ** 2) ** 0.5
+
+    return (sigx, sigy, sigt, emittance_x, emittance_y, emittance_t)
 
 
-# read reduced diagnostics
-rbc = read_time_series("diags/reduced_beam_characteristics.*")
+# initial/final beam
+series = io.Series("diags/openPMD/monitor.h5", io.Access.read_only)
+last_step = list(series.iterations)[-1]
+initial = series.iterations[1].particles["beam"].to_df()
+beam_final = series.iterations[last_step].particles["beam"]
+final = beam_final.to_df()
 
-s = rbc["s"]
-sig_x = rbc["sig_x"]
-sig_y = rbc["sig_y"]
-sig_t = rbc["sig_t"]
-emittance_x = rbc["emittance_x"]
-emittance_y = rbc["emittance_y"]
-emittance_t = rbc["emittance_t"]
-
-sig_xi = sig_x.iloc[0]
-sig_yi = sig_y.iloc[0]
-sig_ti = sig_t.iloc[0]
-emittance_xi = emittance_x.iloc[0]
-emittance_yi = emittance_y.iloc[0]
-emittance_ti = emittance_t.iloc[0]
-
-length = len(s) - 1
-
-sf = s.iloc[length]
-sig_xf = sig_x.iloc[length]
-sig_yf = sig_y.iloc[length]
-sig_tf = sig_t.iloc[length]
-emittance_xf = emittance_x.iloc[length]
-emittance_yf = emittance_y.iloc[length]
-emittance_tf = emittance_t.iloc[length]
+# compare number of particles
+num_particles = 10000
+assert num_particles == len(initial)
+assert num_particles == len(final)
 
 print("Initial Beam:")
+sig_xi, sig_yi, sig_ti, emittance_xi, emittance_yi, emittance_ti = get_moments(initial)
 print(f"  sigx={sig_xi:e} sigy={sig_yi:e} sigt={sig_ti:e}")
 print(
     f"  emittance_x={emittance_xi:e} emittance_y={emittance_yi:e} emittance_t={emittance_ti:e}"
 )
 
 atol = 0.0  # ignored
-rtol = 1.0e-3  # from random sampling of a smooth distribution
+rtol = 2.2 * num_particles**-0.5  # from random sampling of a smooth distribution
 print(f"  rtol={rtol} (ignored: atol~={atol})")
 
 assert np.allclose(
     [sig_xi, sig_yi, sig_ti, emittance_xi, emittance_yi, emittance_ti],
-    [
-        7.500e-05,
-        7.514e-05,
-        0.001,
-        3.899e-07,
-        3.960e-07,
-        0.00039,
-    ],
+    [7.515765e-05, 7.511883e-05, 9.997395e-4, 2.001510e-09, 1.999755e-09, 1.999289e-06],
     rtol=rtol,
     atol=atol,
 )
@@ -91,24 +66,25 @@ assert np.allclose(
 
 print("")
 print("Final Beam:")
+sig_xf, sig_yf, sig_tf, emittance_xf, emittance_yf, emittance_tf = get_moments(initial)
 print(f"  sigx={sig_xf:e} sigy={sig_yf:e} sigt={sig_tf:e}")
 print(
     f"  emittance_x={emittance_xf:e} emittance_y={emittance_yf:e} emittance_t={emittance_tf:e}"
 )
 
 atol = 0.0  # ignored
-rtol = 1.0e-3  # from random sampling of a smooth distribution
+rtol = 2.2 * num_particles**-0.5  # from random sampling of a smooth distribution
 print(f"  rtol={rtol} (ignored: atol~={atol})")
 
 assert np.allclose(
     [sig_xf, sig_yf, sig_tf, emittance_xf, emittance_yf, emittance_tf],
     [
-        9.131e-05,
-        8.616e-05,
-        0.001,
-        3.983e-07,
-        4.040e-07,
-        0.00039,
+        7.51576586332169e-05,
+        7.511883208451813e-05,
+        0.0009997395499750136,
+        2.0015106608723994e-09,
+        1.999755254276969e-09,
+        1.9992898444562777e-06,
     ],
     rtol=rtol,
     atol=atol,

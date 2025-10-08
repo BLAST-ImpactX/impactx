3182852d6dec50f08af06769ada777b4a7318097
Ji Qiang <38738257+qianglbl@users.noreply.github.com>
2025-10-08 15:48:03 -0700

    Update analysis_fodo_Gauss3D_sc.py

--- examples/fodo_space_charge/analysis_fodo_Gauss3D_sc.py
+++ examples/fodo_space_charge/analysis_fodo_Gauss3D_sc.py
@@ -53,7 +53,7 @@ print(
 )
 
 atol = 0.0  # ignored
-rtol = 2.2 * num_particles**-0.5  # from random sampling of a smooth distribution
+rtol = 3 * num_particles**-0.5  # from random sampling of a smooth distribution
 print(f"  rtol={rtol} (ignored: atol~={atol})")
 
 assert np.allclose(
@@ -73,7 +73,7 @@ print(
 )
 
 atol = 0.0  # ignored
-rtol = 2.2 * num_particles**-0.5  # from random sampling of a smooth distribution
+rtol = 3 * num_particles**-0.5  # from random sampling of a smooth distribution
 print(f"  rtol={rtol} (ignored: atol~={atol})")
 
 assert np.allclose(

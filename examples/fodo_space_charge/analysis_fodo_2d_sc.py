bbbbb8d0758733c640d79651f33b4afcf8a56573
Chad Mitchell <46825199+cemitch99@users.noreply.github.com>
2025-10-16 17:53:56 -0700

    Relax tolerance in analysis_fodo_2d_sc.py

--- examples/fodo_space_charge/analysis_fodo_2d_sc.py
+++ examples/fodo_space_charge/analysis_fodo_2d_sc.py
@@ -80,7 +80,7 @@ print(
 )
 
 atol = 0.0  # ignored
-rtol = 4.0 * num_particles**-0.5  # from random sampling of a smooth distribution
+rtol = 5.0 * num_particles**-0.5  # from random sampling of a smooth distribution
 print(f"  rtol={rtol} (ignored: atol~={atol})")
 
 assert np.allclose(

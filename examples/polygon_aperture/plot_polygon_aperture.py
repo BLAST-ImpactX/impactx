ef0c3ce9106d326771e4ae0e6f086623079f6193
Eric G. Stern <egstern@fnal.gov>
2025-11-07 15:30:10 -0600

    New analysis script that checks tests the results of the aperture, small changes to other scripts.

--- examples/polygon_aperture/plot_polygon_aperture.py
+++ examples/polygon_aperture/plot_polygon_aperture.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
-# Copyright 2022-2023 ImpactX contributors
-# Authors: Axel Huebl, Chad Mitchell
+# Copyright 2022-2025 ImpactX contributors
+# Authors: Eric G. Stern, Axel Huebl, Chad Mitchell
 # License: BSD-3-Clause-LBNL
 #
 

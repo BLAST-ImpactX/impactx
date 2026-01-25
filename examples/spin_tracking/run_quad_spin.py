ded6fd75a989f0e421859be5a86e15175bef9fe6
Chad Mitchell <ChadMitchell@lbl.gov>
2026-01-25 12:18:50 -0800

    Update values in Python quad spin test.

--- examples/spin_tracking/run_quad_spin.py
+++ examples/spin_tracking/run_quad_spin.py
@@ -22,41 +22,31 @@ sim.slice_step_diagnostics = True
 sim.init_grids()
 
 # basic beam parameters
-total_energy_MeV = 100.0  # reference energy (total)
+kin_energy_MeV = 100.0 # reference energy (kinetic)
 mass_MeV = 0.510998950  # particle mass
-kin_energy_MeV = total_energy_MeV - mass_MeV
 bunch_charge_C = 25.0e-12  # used with space charge
-npart = 10000  # number of macro particles
+npart = 100000  # number of macro particles
 
 # set reference particle
 ref = sim.particle_container().ref_particle()
 ref.set_charge_qe(-1.0).set_mass_MeV(mass_MeV).set_kin_energy_MeV(kin_energy_MeV)
 
-# factors converting the beam distribution to ImpactX input
-gamma = total_energy_MeV / mass_MeV
-bg = np.sqrt(gamma**2 - 1.0)
-sigma_tau = 1e-6  # in m
-sigma_p = 2.5e-2  # dimensionless
-rigidity = m_e * c * bg / e
-
 #   particle bunch
-distr = distribution.Triangle(
-    **twiss(
-        beta_x=0.002,
-        beta_y=0.002,
-        beta_t=sigma_tau / sigma_p,
-        emitt_x=1.5e-6 / bg,
-        emitt_y=1.5e-6 / bg,
-        emitt_t=sigma_tau * sigma_p,
-        alpha_x=0.0,
-        alpha_y=0.0,
-        alpha_t=0.0,
-    )
+distr = distribution.Gaussian(
+        lambdaX=0.0003,
+        lambdaY=0.0003,
+        lambdaT=1.0e-6,
+        lambdaPx=0.0002,
+        lambdaPy=0.0002,
+        lambdaPt=1.0e-6,
+        muxpx=0.0,
+        muypy=0.0,
+        mutpt=0.0,
 )
 spin = distribution.SpinvMF(
-    0.7,
-    0.0,
-    0.0,
+    0.4,
+    0.9,
+    0.1,
 )
 
 sim.add_particles(bunch_charge_C, distr, npart, spin)
@@ -67,10 +57,17 @@ ns = 1  # number of slices per ds in the element
 # add beam diagnostics
 monitor = elements.BeamMonitor("monitor", backend="h5")
 
-# element names consistent with HTU_base_lattice_matched.lte Elegant input
+# quad focusing strength (> 0)
+k_value = 100.0
+print("k_value")
+print(k_value)
 
-drift1 = elements.ExactDrift(name="drift1", ds=0.046, nslice=ns)
-quad1 = elements.Quad(name="quad1", ds=0.02903, k=207.0, nslice=ns)
+# length for this test should be one period
+ds_value = 2.0*np.pi / np.sqrt(k_value)
+
+quad1 = elements.Quad(
+    name="quad1", ds=ds_value, k=k_value, nslice=ns
+)
 
 # set the lattice
 sim.lattice.append(monitor)

b212af1af48f2fd746b5e94dd1e22215551361da
Axel Huebl <axel.huebl@plasma.ninja>
2026-02-09 08:18:56 -0800

    Release 26.02 (#1298)
    * Release 26.02
    
    The February release of ImpactX :)
    
    * CI: Win OpenMP FFTW
    
    * Tempoary: AMReX_FFTW_IGNORE_OMP=ON
    
    Reason: CMake Config OpenMP support missing.
    
    * [Patch] Try AMReX Fix
    
    https://github.com/AMReX-Codes/amrex/pull/4940

--- setup.py
+++ setup.py
@@ -242,7 +242,7 @@ with open("./requirements.txt") as f:
 setup(
     name="impactx",
     # note PEP-440 syntax: x.y.zaN but x.y.z.devN
-    version="26.01",
+    version="26.02",
     packages=["impactx"],
     # Python sources:
     package_dir={"": "src/python"},

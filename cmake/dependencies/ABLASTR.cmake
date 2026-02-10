249b5a060d949a8e5eb8baee7a1a7f8afbb8778b
Axel Huebl <axel.huebl@plasma.ninja>
2026-02-09 22:38:54 -0800

    AMReX `development` (#1300)
    * CI: AMReX Unpatch
    
    * AMReX: `development`

--- cmake/dependencies/ABLASTR.cmake
+++ cmake/dependencies/ABLASTR.cmake
@@ -186,7 +186,7 @@ set(ImpactX_ablastr_branch "26.02"
 set(ImpactX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(ImpactX_amrex_internal)")
-set(ImpactX_amrex_branch "26.02"
+set(ImpactX_amrex_branch "d7da504589664922ce3e21972782713d31ec50e9"
     CACHE STRING
     "Repository branch for ImpactX_amrex_repo if(ImpactX_amrex_internal)")
 

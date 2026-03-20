63f4514af035dac8dbf7047ac67c00aa97c21824
Axel Huebl <axel.huebl@plasma.ninja>
2026-03-20 16:10:41 -0500

    MPI: Add Particles Once

--- examples/spin_tracking/run_exact_quad_spin_scaling.py
+++ examples/spin_tracking/run_exact_quad_spin_scaling.py
@@ -38,76 +38,77 @@ qm_eev = 1.0 / 0.510998950 / 1e6  # electron charge/mass in e / eV
 
 pc = sim.particle_container()
 
-df_initial = pd.read_csv("./initial_coords.csv", sep=" ")
-dx = df_initial["x"].to_numpy()
-dpx = df_initial["px"].to_numpy()
-dy = df_initial["y"].to_numpy()
-dpy = df_initial["py"].to_numpy()
-dt = df_initial["t"].to_numpy()
-dpt = df_initial["pt"].to_numpy()
-dw = df_initial["w"].to_numpy()
-dsx = df_initial["sx"].to_numpy()
-dsy = df_initial["sy"].to_numpy()
-dsz = df_initial["sz"].to_numpy()
-if not Config.have_gpu:  # initialize using cpu-based PODVectors
-    dx_podv = amr.PODVector_real_std()
-    dy_podv = amr.PODVector_real_std()
-    dt_podv = amr.PODVector_real_std()
-    dpx_podv = amr.PODVector_real_std()
-    dpy_podv = amr.PODVector_real_std()
-    dpt_podv = amr.PODVector_real_std()
-    dw_podv = amr.PODVector_real_std()
-    dsx_podv = amr.PODVector_real_std()
-    dsy_podv = amr.PODVector_real_std()
-    dsz_podv = amr.PODVector_real_std()
+if amr.ParallelDescriptor.IOProcessor():
+    df_initial = pd.read_csv("./initial_coords.csv", sep=" ")
+    dx = df_initial["x"].to_numpy()
+    dpx = df_initial["px"].to_numpy()
+    dy = df_initial["y"].to_numpy()
+    dpy = df_initial["py"].to_numpy()
+    dt = df_initial["t"].to_numpy()
+    dpt = df_initial["pt"].to_numpy()
+    dw = df_initial["w"].to_numpy()
+    dsx = df_initial["sx"].to_numpy()
+    dsy = df_initial["sy"].to_numpy()
+    dsz = df_initial["sz"].to_numpy()
+    if not Config.have_gpu:  # initialize using cpu-based PODVectors
+        dx_podv = amr.PODVector_real_std()
+        dy_podv = amr.PODVector_real_std()
+        dt_podv = amr.PODVector_real_std()
+        dpx_podv = amr.PODVector_real_std()
+        dpy_podv = amr.PODVector_real_std()
+        dpt_podv = amr.PODVector_real_std()
+        dw_podv = amr.PODVector_real_std()
+        dsx_podv = amr.PODVector_real_std()
+        dsy_podv = amr.PODVector_real_std()
+        dsz_podv = amr.PODVector_real_std()
 
-else:  # initialize on device using arena/gpu-based PODVectors
-    dx_podv = amr.PODVector_real_arena()
-    dy_podv = amr.PODVector_real_arena()
-    dt_podv = amr.PODVector_real_arena()
-    dpx_podv = amr.PODVector_real_arena()
-    dpy_podv = amr.PODVector_real_arena()
-    dpt_podv = amr.PODVector_real_arena()
-    dw_podv = amr.PODVector_real_arena()
-    dsx_podv = amr.PODVector_real_arena()
-    dsy_podv = amr.PODVector_real_arena()
-    dsz_podv = amr.PODVector_real_arena()
+    else:  # initialize on device using arena/gpu-based PODVectors
+        dx_podv = amr.PODVector_real_arena()
+        dy_podv = amr.PODVector_real_arena()
+        dt_podv = amr.PODVector_real_arena()
+        dpx_podv = amr.PODVector_real_arena()
+        dpy_podv = amr.PODVector_real_arena()
+        dpt_podv = amr.PODVector_real_arena()
+        dw_podv = amr.PODVector_real_arena()
+        dsx_podv = amr.PODVector_real_arena()
+        dsy_podv = amr.PODVector_real_arena()
+        dsz_podv = amr.PODVector_real_arena()
 
-for p_dx in dx:
-    dx_podv.push_back(p_dx)
-for p_dy in dy:
-    dy_podv.push_back(p_dy)
-for p_dt in dt:
-    dt_podv.push_back(p_dt)
-for p_dpx in dpx:
-    dpx_podv.push_back(p_dpx)
-for p_dpy in dpy:
-    dpy_podv.push_back(p_dpy)
-for p_dpt in dpt:
-    dpt_podv.push_back(p_dpt)
-for p_dw in dw:
-    dw_podv.push_back(p_dw)
-for p_dsx in dsx:
-    dsx_podv.push_back(p_dsx)
-for p_dsy in dsy:
-    dsy_podv.push_back(p_dsy)
-for p_dsz in dsz:
-    dsz_podv.push_back(p_dsz)
+    for p_dx in dx:
+        dx_podv.push_back(p_dx)
+    for p_dy in dy:
+        dy_podv.push_back(p_dy)
+    for p_dt in dt:
+        dt_podv.push_back(p_dt)
+    for p_dpx in dpx:
+        dpx_podv.push_back(p_dpx)
+    for p_dpy in dpy:
+        dpy_podv.push_back(p_dpy)
+    for p_dpt in dpt:
+        dpt_podv.push_back(p_dpt)
+    for p_dw in dw:
+        dw_podv.push_back(p_dw)
+    for p_dsx in dsx:
+        dsx_podv.push_back(p_dsx)
+    for p_dsy in dsy:
+        dsy_podv.push_back(p_dsy)
+    for p_dsz in dsz:
+        dsz_podv.push_back(p_dsz)
 
-pc.add_n_particles(
-    dx_podv,
-    dy_podv,
-    dt_podv,
-    dpx_podv,
-    dpy_podv,
-    dpt_podv,
-    qm_eev,
-    bunch_charge_C,
-    dw_podv,
-    dsx_podv,
-    dsy_podv,
-    dsz_podv,
-)
+    pc.add_n_particles(
+        dx_podv,
+        dy_podv,
+        dt_podv,
+        dpx_podv,
+        dpy_podv,
+        dpt_podv,
+        qm_eev,
+        bunch_charge_C,
+        dw_podv,
+        dsx_podv,
+        dsy_podv,
+        dsz_podv,
+    )
 
 # add beam diagnostics
 monitor = elements.BeamMonitor("monitor", backend="h5", period_sample_intervals=10)

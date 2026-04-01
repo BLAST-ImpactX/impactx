4e0763f252dd69cb8d29763015761fcd94a33f64
ax3l <1353258+ax3l@users.noreply.github.com>
2026-04-01 18:31:12 +0000

    Update Stub Files

--- src/python/impactx/extensions/KnownElementsList.pyi
+++ src/python/impactx/extensions/KnownElementsList.pyi
@@ -292,4 +292,4 @@ _DRIFT_MODEL_CLASSES: dict = {
 }
 _filtered_views_by_lattice: (
     weakref.WeakKeyDictionary
-)  # value = <WeakKeyDictionary at 0x7f6872916cf0>
+)  # value = <WeakKeyDictionary at 0x7f30dff1acf0>

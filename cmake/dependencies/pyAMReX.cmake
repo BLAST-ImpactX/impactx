b5180815015e4bd63581e71e3a001d5b14d6fd15
Axel Huebl <axel.huebl@plasma.ninja>
2026-04-01 13:34:06 -0700

    ABLASTR/(py)AMReX: `development`

--- cmake/dependencies/pyAMReX.cmake
+++ cmake/dependencies/pyAMReX.cmake
@@ -80,7 +80,7 @@ option(ImpactX_pyamrex_internal "Download & build pyAMReX" ON)
 set(ImpactX_pyamrex_repo "https://github.com/AMReX-Codes/pyamrex.git"
     CACHE STRING
     "Repository URI to pull and build pyamrex from if(ImpactX_pyamrex_internal)")
-set(ImpactX_pyamrex_branch "26.03"
+set(ImpactX_pyamrex_branch "edf12bfc6ab5426a4b206e40f988afbadb93d437"
     CACHE STRING
     "Repository branch for ImpactX_pyamrex_repo if(ImpactX_pyamrex_internal)")
 

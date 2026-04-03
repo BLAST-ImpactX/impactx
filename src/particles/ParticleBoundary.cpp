562fd4beec370d343012b759cf63af8bcc26077b
Chad Mitchell <ChadMitchell@lbl.gov>
2026-04-03 10:42:21 -0700

    Add algo string parsing.

--- src/particles/ParticleBoundary.cpp
+++ src/particles/ParticleBoundary.cpp
@@ -25,46 +25,48 @@ namespace impactx::particles
         using namespace amrex::literals;
 
         amrex::ParmParse pp_algo("algo");
-        std::string particle_bc = "periodic";
+        std::string particle_bc = "off";
         pp_algo.queryAdd("particle_bc", particle_bc);
 
-        // Loop over refinement levels
-        int const nLevel = pc.finestLevel();
-        for (int lev = 0; lev <= nLevel; ++lev)
-        {
-            // Loop over all particle boxes
-            using ParIt = ImpactXParticleContainer::iterator;
+        if(particle_bc == "periodic") {
+            // Loop over refinement levels
+            int const nLevel = pc.finestLevel();
+            for (int lev = 0; lev <= nLevel; ++lev)
+            {
+                // Loop over all particle boxes
+                using ParIt = ImpactXParticleContainer::iterator;
 
 #ifdef AMREX_USE_OMP
 #pragma omp parallel if (amrex::Gpu::notInLaunchRegion())
 #endif
-            for (ParIt pti(pc, lev); pti.isValid(); ++pti)
-            {
-                const int np = pti.numParticles();
-
-                // Access bucket length and reference particle quantities.
-                amrex::ParticleReal const bucket_length = pc.GetBucketLength();
-                amrex::ParticleReal const ref_beta = pc.GetRefParticle().beta();
-                amrex::ParticleReal const bucket_duration = (ref_beta != 0.0)? bucket_length / ref_beta : 0.0;
-                amrex::ParticleReal const bucket_half_duration = bucket_duration / 2.0;
-
-                // Access data from StructOfArrays (soa)
-                auto& soa_real = pti.GetStructOfArrays().GetRealData();
-
-                amrex::ParticleReal* const AMREX_RESTRICT part_t = soa_real[RealSoA::t].dataPtr();
-
-                // Gather particles and apply boundary condition
-                amrex::ParallelFor(np, [=] AMREX_GPU_DEVICE (int i)
+                for (ParIt pti(pc, lev); pti.isValid(); ++pti)
                 {
-                    // Access SoA Real data
-                    amrex::ParticleReal & AMREX_RESTRICT t = part_t[i];
+                    const int np = pti.numParticles();
 
-                    // Implement phase wrapping in t
-                    amrex::ParticleReal ttest = std::fmod(t+bucket_half_duration, bucket_duration);
-                    t = (bucket_duration != 0.0)? std::fmod(ttest+bucket_duration, bucket_duration)-bucket_half_duration : t;
+                    // Access bucket length and reference particle quantities.
+                    amrex::ParticleReal const bucket_length = pc.GetBucketLength();
+                    amrex::ParticleReal const ref_beta = pc.GetRefParticle().beta();
+                    amrex::ParticleReal const bucket_duration = (ref_beta != 0.0)? bucket_length / ref_beta : 0.0;
+                    amrex::ParticleReal const bucket_half_duration = bucket_duration / 2.0;
 
-                });
-            } // End loop over all particle boxes
-        } // End mesh-refinement level loop
+                    // Access data from StructOfArrays (soa)
+                    auto& soa_real = pti.GetStructOfArrays().GetRealData();
+
+                    amrex::ParticleReal* const AMREX_RESTRICT part_t = soa_real[RealSoA::t].dataPtr();
+
+                    // Gather particles and apply boundary condition
+                    amrex::ParallelFor(np, [=] AMREX_GPU_DEVICE (int i)
+                    {
+                        // Access SoA Real data
+                        amrex::ParticleReal & AMREX_RESTRICT t = part_t[i];
+
+                        // Implement phase wrapping in t
+                        amrex::ParticleReal ttest = std::fmod(t+bucket_half_duration, bucket_duration);
+                        t = (bucket_duration != 0.0)? std::fmod(ttest+bucket_duration, bucket_duration)-bucket_half_duration : t;
+
+                    });
+                } // End loop over all particle boxes
+            } // End mesh-refinement level loop
+        }
     }
 } // namespace impactx::particles

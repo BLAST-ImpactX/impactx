dd2b391663281ba6bf08579c9eeb630f69dff7a7
Chad Mitchell <ChadMitchell@lbl.gov>
2026-04-16 12:55:55 -0700

    Add a second example.

--- examples/phase_wrapper/run_rf_phase_wrap.py
+++ examples/phase_wrapper/run_rf_phase_wrap.py
@@ -24,25 +24,25 @@ sim.init_grids()
 # load bunch parameters
 kin_energy_MeV = 10.0e3  # reference energy
 bunch_charge_C = 1.0e-9  # used with space charge
-npart = 10000  # number of macro particles
+npart = 100000  # number of macro particles
 
 #   reference particle
 ref = sim.particle_container().ref_particle()
-ref.set_species("electron").set_kin_energy_MeV(kin_energy_MeV)
+ref.set_species("proton").set_kin_energy_MeV(kin_energy_MeV)
 
 #   set bunch bucket length
 sim.particle_container().set_bucket_length(0.23)
 
 #   particle bunch
 distr = distribution.Gaussian(
-    lambdaX=5.0e-4,
-    lambdaY=5.0e-4,
-    lambdaT=0.2,
-    lambdaPx=0.0,
-    lambdaPy=0.0,
-    lambdaPt=0.0,
-    muxpx=0.0,
-    muypy=0.0,
+    lambdaX=3.9984884770e-5,
+    lambdaY=3.9984884770e-5,
+    lambdaT=5.0e-2,
+    lambdaPx=2.6623538760e-5,
+    lambdaPy=2.6623538760e-5,
+    lambdaPt=0.0e-2,
+    muxpx=-0.846574929020762,
+    muypy=0.846574929020762,
     mutpt=0.0,
 )
 sim.add_particles(bunch_charge_C, distr, npart)
@@ -51,14 +51,19 @@ sim.add_particles(bunch_charge_C, distr, npart)
 monitor = elements.BeamMonitor("monitor", backend="h5")
 
 # design the accelerator lattice)
-ns = 1  # number of slices per ds in the element
+ns = 10  # number of slices per ds in the element
 line = [
     monitor,
-    elements.Drift(name="drift1", ds=0.0, nslice=ns),
+    elements.Drift(name="drift1", ds=0.5, nslice=ns),
+    elements.ShortRF(name="shortrf1", V=10.0, freq=1.3e9, phase=-60.0),
+    elements.Drift(name="drift1", ds=0.5, nslice=ns),
     monitor,
 ]
 # assign the lattice
 sim.lattice.extend(line)
+
+# number of periods
+sim.periods = 10
 
 # run simulation
 sim.track_particles()

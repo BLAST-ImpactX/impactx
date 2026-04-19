cfe3d8b8b3949a10cd4594e0b8e746cb673562bc
Chad Mitchell <ChadMitchell@lbl.gov>
2026-04-19 12:52:42 -0700

    Simplify SoftSol example.

--- examples/spin_tracking/run_solenoid_softedge_solvable_spin.py
+++ examples/spin_tracking/run_solenoid_softedge_solvable_spin.py
@@ -15,65 +15,17 @@ sim = ImpactX()
 
 # set numerical parameters and IO control
 sim.space_charge = False
-# sim.diagnostics = False  # benchmarking
 sim.slice_step_diagnostics = False
 
 # domain decomposition & space charge mesh
 sim.init_grids()
 
-# load a 250 MeV proton beam with an initial
-# horizontal rms emittance of 1 um and an
-# initial vertical rms emittance of 2 um
+# reference kinetic energy
 kin_energy_MeV = 250.0  # reference energy
-bunch_charge_C = 1.0e-9  # used with space charge
 
 #   reference particle
 ref = sim.particle_container().ref_particle()
 ref.set_species("proton").set_kin_energy_MeV(kin_energy_MeV)
-qm_eev = 1.0 / 938.27208816 / 1e6  # electron charge/mass in e / eV
-
-#   particle bunch
-pc = sim.particle_container()
-
-dx = [1, 0, 0, 0, 0, 0]
-dpx = [0, 1, 0, 0, 0, 0]
-dy = [0, 0, 1, 0, 0, 0]
-dpy = [0, 0, 0, 1, 0, 0]
-dt = [0, 0, 0, 0, 1, 0]
-dpt = [0, 0, 0, 0, 0, 1]
-
-if not Config.have_gpu:  # initialize using cpu-based PODVectors
-    dx_podv = amr.PODVector_real_std()
-    dy_podv = amr.PODVector_real_std()
-    dt_podv = amr.PODVector_real_std()
-    dpx_podv = amr.PODVector_real_std()
-    dpy_podv = amr.PODVector_real_std()
-    dpt_podv = amr.PODVector_real_std()
-else:  # initialize on device using arena/gpu-based PODVectors
-    dx_podv = amr.PODVector_real_arena()
-    dy_podv = amr.PODVector_real_arena()
-    dt_podv = amr.PODVector_real_arena()
-    dpx_podv = amr.PODVector_real_arena()
-    dpy_podv = amr.PODVector_real_arena()
-    dpt_podv = amr.PODVector_real_arena()
-
-for p_dx in dx:
-    dx_podv.push_back(p_dx)
-for p_dy in dy:
-    dy_podv.push_back(p_dy)
-for p_dt in dt:
-    dt_podv.push_back(p_dt)
-for p_dpx in dpx:
-    dpx_podv.push_back(p_dpx)
-for p_dpy in dpy:
-    dpy_podv.push_back(p_dpy)
-for p_dpt in dpt:
-    dpt_podv.push_back(p_dpt)
-
-if amr.ParallelDescriptor.IOProcessor():
-    pc.add_n_particles(
-        dx_podv, dy_podv, dt_podv, dpx_podv, dpy_podv, dpt_podv, qm_eev, bunch_charge_C
-    )
 
 # specify the on-axis field profile
 zmin = -1.0  # lower value of on-axis longitudinal coordinate (in meters)
@@ -95,24 +47,20 @@ sol = elements.SoftSolenoid(
     nslice=1,
 )
 
+# lattice
+sim.lattice.extend(
+    [
+        sol,
+    ]
+)
+
+# run simulation 
+sim.track_reference(ref) 
+
 # return linear map
 Rmat = Map6x6()
 vmat = Vector3()
 Amat = Map3x6()
-
-# add beam diagnostics
-monitor = elements.BeamMonitor("monitor", backend="h5")
-
-sim.lattice.extend(
-    [
-        monitor,
-        sol,
-        monitor,
-    ]
-)
-
-# run simulation
-sim.track_particles()
 
 Rmat = ref.map
 vmat = ref.spin_rotation_vector

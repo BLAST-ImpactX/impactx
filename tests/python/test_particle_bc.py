b6cc6bb604e8248b10b60a79fe6754685ad6c813
Axel Huebl <axel.huebl@plasma.ninja>
2026-04-20 21:16:15 -0700

    Test: Deduplicate Shared Logic

--- tests/python/test_particle_bc.py
+++ tests/python/test_particle_bc.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 #
-# Copyright 2022-2023 The ImpactX Community
+# Copyright 2022-2026 The ImpactX Community
 #
-# Authors: Axel Huebl
+# Authors: Chad Mitchell, Axel Huebl
 # License: BSD-3-Clause-LBNL
 #
 # -*- coding: utf-8 -*-
@@ -12,34 +12,32 @@ import numpy as np
 from impactx import ImpactX, distribution, elements
 
 
-def test_particle_bc_periodic():
-    """
-    This tests the application of longitudinal particle boundary conditions.
-    """
+def _track_particle_bc(particle_bc, lattice):
     sim = ImpactX()
 
     sim.particle_shape = 2
     sim.slice_step_diagnostics = False
-    sim.particle_bc = "periodic"
+    sim.particle_bc = particle_bc
     sim.init_grids()
 
     # init particle beam
     kin_energy_MeV = 10.0e3
     bunch_charge_C = 1.0e-9
     npart = 10000
+    bucket_length = 0.2
 
     #   reference particle
     ref = sim.particle_container().ref_particle()
     ref.set_species("electron").set_kin_energy_MeV(kin_energy_MeV)
 
     #   set bunch bucket length
-    sim.particle_container().set_bucket_length(0.2)
+    sim.particle_container().set_bucket_length(bucket_length)
 
     #   particle bunch
     distr = distribution.Gaussian(
         lambdaX=5.0e-4,
         lambdaY=5.0e-4,
-        lambdaT=0.2,
+        lambdaT=bucket_length,
         lambdaPx=0.0,
         lambdaPy=0.0,
         lambdaPt=0.0,
@@ -53,18 +51,23 @@ def test_particle_bc_periodic():
     assert pc.total_number_of_particles() == npart
 
     # init accelerator lattice
-    monitor = elements.BeamMonitor("monitor", backend="h5")
-
-    lattice = [monitor, monitor]
     sim.lattice.extend(lattice)
 
     sim.track_particles()
 
+    return pc, bucket_length
+
+
+def test_particle_bc_periodic():
+    """
+    This tests the application of longitudinal particle boundary conditions.
+    """
+    monitor = elements.BeamMonitor("monitor", backend="h5")
+    pc, bucket_length = _track_particle_bc("periodic", [monitor, monitor])
+
     # access particle data
     [xmin, ymin, zmin, xmax, ymax, zmax] = pc.min_and_max_positions()
 
-    # numerical parameters
-    bucket_length = 0.2
     half_bucket_length = bucket_length / 2.0
 
     # check that all final particle lie within the bucket
@@ -75,55 +78,12 @@ def test_particle_bc_absorbing():
     """
     This tests the application of longitudinal particle boundary conditions.
     """
-    sim = ImpactX()
-
-    sim.particle_shape = 2
-    sim.slice_step_diagnostics = False
-    sim.particle_bc = "absorbing"
-    sim.init_grids()
-
-    # init particle beam
-    kin_energy_MeV = 10.0e3
-    bunch_charge_C = 1.0e-9
-    npart = 10000
-
-    #   reference particle
-    ref = sim.particle_container().ref_particle()
-    ref.set_species("electron").set_kin_energy_MeV(kin_energy_MeV)
-
-    #   set bunch bucket length
-    sim.particle_container().set_bucket_length(0.2)
-
-    #   particle bunch
-    distr = distribution.Gaussian(
-        lambdaX=5.0e-4,
-        lambdaY=5.0e-4,
-        lambdaT=0.2,
-        lambdaPx=0.0,
-        lambdaPy=0.0,
-        lambdaPt=0.0,
-        muxpx=0.0,
-        muypy=0.0,
-        mutpt=0.0,
-    )
-    sim.add_particles(bunch_charge_C, distr, npart)
-
-    pc = sim.particle_container()
-    assert pc.total_number_of_particles() == npart
-
-    # init accelerator lattice
     monitor = elements.BeamMonitor("monitor", backend="h5")
-
-    lattice = [monitor, monitor]
-    sim.lattice.extend(lattice)
-
-    sim.track_particles()
+    pc, bucket_length = _track_particle_bc("absorbing", [monitor, monitor])
 
     # access particle data
     [xmin, ymin, zmin, xmax, ymax, zmax] = pc.min_and_max_positions()
 
-    # numerical parameters
-    bucket_length = 0.2
     half_bucket_length = bucket_length / 2.0
 
     # check that all final particle lie within the bucket
@@ -134,56 +94,13 @@ def test_particle_bc_reflecting():
     """
     This tests the application of longitudinal particle boundary conditions.
     """
-    sim = ImpactX()
-
-    sim.particle_shape = 2
-    sim.slice_step_diagnostics = False
-    sim.particle_bc = "reflecting"
-    sim.init_grids()
-
-    # init particle beam
-    kin_energy_MeV = 10.0e3
-    bunch_charge_C = 1.0e-9
-    npart = 10000
-
-    #   reference particle
-    ref = sim.particle_container().ref_particle()
-    ref.set_species("electron").set_kin_energy_MeV(kin_energy_MeV)
-
-    #   set bunch bucket length
-    sim.particle_container().set_bucket_length(0.2)
-
-    #   particle bunch
-    distr = distribution.Gaussian(
-        lambdaX=5.0e-4,
-        lambdaY=5.0e-4,
-        lambdaT=0.2,
-        lambdaPx=0.0,
-        lambdaPy=0.0,
-        lambdaPt=0.0,
-        muxpx=0.0,
-        muypy=0.0,
-        mutpt=0.0,
-    )
-    sim.add_particles(bunch_charge_C, distr, npart)
-
-    pc = sim.particle_container()
-    assert pc.total_number_of_particles() == npart
-
-    # init accelerator lattice
     monitor = elements.BeamMonitor("monitor", backend="h5")
     drift = elements.Drift(ds=0.0, nslice=2)
-
-    lattice = [monitor, drift, monitor]
-    sim.lattice.extend(lattice)
-
-    sim.track_particles()
+    pc, bucket_length = _track_particle_bc("reflecting", [monitor, drift, monitor])
 
     # access particle data
     [xmin, ymin, zmin, xmax, ymax, zmax] = pc.min_and_max_positions()
 
-    # numerical parameters
-    bucket_length = 0.2
     half_bucket_length = bucket_length / 2.0
 
     # check that all final particle lie within the bucket

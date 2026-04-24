14dfba0a90a14dacb09027be75ba499ad6f8ae64
Axel Huebl <axel.huebl@plasma.ninja>
2026-04-24 15:51:24 -0700

    Cleanup: Resolved TODOs

--- examples/simple_booster/analysis_simple_booster.py
+++ examples/simple_booster/analysis_simple_booster.py
@@ -54,20 +54,6 @@ assert math.isclose(total_s, expected_s, rel_tol=1.0e-4), (
     f"lattice length {total_s} m does not match expected {expected_s} m"
 )
 
-# initial/final beam (TODO)
-# series = io.Series("diags/openPMD/monitor.h5", io.Access.read_only)
-# last_step = list(series.iterations)[-1]
-# initial = series.iterations[1].particles["beam"].to_df()
-# final = series.iterations[last_step].particles["beam"].to_df()
-
-# compare number of particles (TODO)
-# num_particles = 10000
-# assert num_particles == len(initial)
-# assert num_particles == len(final)
-
-# TODO for Eric: add more tests as needed, e.g., checking beam moments
-
-
 series = io.Series("diags/openPMD/monitor.h5", io.Access.read_only)
 last_step = list(series.iterations)[-1]
 initial = series.iterations[1].particles["beam"].to_df()

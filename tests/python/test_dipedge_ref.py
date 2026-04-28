788e59cf0297e07b336f13288f00901ed1f662fb
Chad Mitchell <ChadMitchell@lbl.gov>
2026-04-27 17:02:05 -0700

    Add previously-neglected referece shift in tout.

--- tests/python/test_dipedge_ref.py
+++ tests/python/test_dipedge_ref.py
@@ -113,7 +113,7 @@ def test_dipedge_modify_ref_part_false():
     """
     edge_angle = math.pi / 8.0
     rc = 10.0
-    g = 1.0e-3
+    g = 1.0e-2
 
     K0 = np.pi**2 / 6.0
     c2 = (1.0 / np.cos(edge_angle)) ** 3 * np.sin(edge_angle) / 2.0 * g**2 / rc**2 * K0
@@ -121,10 +121,11 @@ def test_dipedge_modify_ref_part_false():
     c5 = np.tan(edge_angle) / (2.0 * rc)
     c13 = 0.0
 
+    rbc, ref = _track_particle_dipedge(False, edge_angle, g, rc)
+
     x_shift = -c3
     px_shift = c13 - c2 - c3 * c5
-
-    rbc, ref = _track_particle_dipedge(False, edge_angle, g, rc)
+    t_shift = -c3 * px_shift / ref.beta
 
     # access centroid data (= single particle orbit)
     vec_part = np.array(
@@ -137,14 +138,16 @@ def test_dipedge_modify_ref_part_false():
             rbc["mean_pt"],
         ]
     )
-    vec_part_pred = [x_shift, px_shift, 0.0, 0.0, 0.0, 0.0]
+    vec_part_pred = [x_shift, px_shift, 0.0, 0.0, t_shift, 0.0]
 
     # access reference particle
-    vec_ref = np.array([ref.x, ref.px, ref.y, ref.py])
-    vec_ref_pred = [0, 0, 0, 0]
+    beta = ref.beta
+    vec_ref = np.array([ref.x, ref.px, ref.y, ref.py, ref.t])
+    vec_ref_pred = [0, 0, 0, 0, 0]
     vec_ref_on_shell = (
         (ref.px) ** 2 + (ref.py) ** 2 + (ref.pz) ** 2 - (ref.pt) ** 2 + 1.0
     )
+
     atol = 1.0e-12
     np.testing.assert_allclose(vec_part, vec_part_pred, atol=atol)
     np.testing.assert_allclose(vec_ref, vec_ref_pred, atol=atol)
@@ -157,17 +160,19 @@ def test_dipedge_modify_ref_part_true():
     """
     edge_angle = math.pi / 8.0
     rc = 10.0
-    g = 1.0e-3
+    g = 1.0e-2
 
     K0 = np.pi**2 / 6.0
     c2 = (1.0 / np.cos(edge_angle)) ** 3 * np.sin(edge_angle) / 2.0 * g**2 / rc**2 * K0
     c3 = (1.0 / np.cos(edge_angle)) ** 2 * g**2 / rc * K0
     c5 = np.tan(edge_angle) / (2.0 * rc)
     c13 = 0.0
-    x_shift = -c3
-    px_shift = c13 - c2 - c3 * c5
 
     rbc, ref = _track_particle_dipedge(True, edge_angle, g, rc)
+
+    x_shift = -c3
+    px_shift = c13 - c2 - c3 * c5
+    t_shift = -c3 * px_shift / ref.beta
 
     # access centroid data (= single particle orbit)
     vec_part = np.array(
@@ -183,8 +188,8 @@ def test_dipedge_modify_ref_part_true():
     vec_part_pred = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
     # access reference particle
-    vec_ref = np.array([ref.x, ref.px, ref.y, ref.py])
-    vec_ref_pred = [x_shift, px_shift, 0, 0]
+    vec_ref = np.array([ref.x, ref.px, ref.y, ref.py, ref.t])
+    vec_ref_pred = [x_shift, px_shift, 0, 0, t_shift]
     vec_ref_on_shell = (
         (ref.px) ** 2 + (ref.py) ** 2 + (ref.pz) ** 2 - (ref.pt) ** 2 + 1.0
     )

ab9bcfd95532afd060ba235c9576bcffdd4800d6
Chad Mitchell <46825199+cemitch99@users.noreply.github.com>
2026-05-21 09:33:28 -0700

    Try reducing blocking factor.

--- examples/expanding_beam/run_expanding_fft_2D_test_particles.py
+++ examples/expanding_beam/run_expanding_fft_2D_test_particles.py
@@ -18,8 +18,8 @@ sim = ImpactX()
 # set numerical parameters and IO control
 sim.max_level = 0
 sim.n_cell = [32, 32, 1]
-sim.blocking_factor_x = [32]
-sim.blocking_factor_y = [32]
+sim.blocking_factor_x = [8]
+sim.blocking_factor_y = [8]
 sim.blocking_factor_z = [1]
 
 sim.particle_shape = 2  # B-spline order

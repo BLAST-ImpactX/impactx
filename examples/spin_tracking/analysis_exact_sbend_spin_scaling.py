a0d568a294d0862a579fb351a913ee71ba17ef63
Chad Mitchell <46825199+cemitch99@users.noreply.github.com>
2026-05-22 13:08:03 -0700

    Spin push for ExactSbend (#1448)
    * Add skeleton of ExactSbend+spin.
    
    * Add second map factor update.
    
    * Small fixes to push.
    
    * Address T_Real initialization error.
    
    * Draft of scaling test.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix a typo in lambda update.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add example documentation.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update Docs: Header, Cross-Link
    
    * Fix: C `sqrt` Usage
    
    * Strengthen benchmark scaling test.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Address Codex comments, part II.
    
    * Fix typo
    
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>

---
+++ examples/spin_tracking/analysis_exact_sbend_spin_scaling.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022-2023 ImpactX contributors
+# Authors: Axel Huebl, Chad Mitchell
+# License: BSD-3-Clause-LBNL
+#
+
+import numpy as np
+import openpmd_api as io
+
+# initial/final beam
+series = io.Series("diags/openPMD/monitor.h5", io.Access.read_only)
+last_step = list(series.iterations)[-1]
+initial = series.iterations[1].particles["beam"].to_df()
+final = series.iterations[last_step].particles["beam"].to_df()
+
+# initial coordinates
+xi = initial["position_x"]
+pxi = initial["momentum_x"]
+yi = initial["position_y"]
+pyi = initial["momentum_y"]
+ti = initial["position_t"]
+pti = initial["momentum_t"]
+sxi = initial["spin_x"]
+syi = initial["spin_y"]
+szi = initial["spin_z"]
+norm_phase_space_vector = np.sqrt(xi**2 + pxi**2 + yi**2 + pyi**2 + ti**2 + pti**2)
+
+# difference of final coordinates from linear values
+diff_xf = final["position_x"] - xi
+diff_pxf = final["momentum_x"] - pxi
+diff_yf = final["position_y"] - yi
+diff_pyf = final["momentum_y"] - pyi
+diff_tf = final["position_t"] - ti
+diff_ptf = final["momentum_t"] - pti
+diff_sxf = final["spin_x"] - sxi
+diff_syf = final["spin_y"] - syi
+diff_szf = final["spin_z"] - szi
+norm_phase_space_diff = np.sqrt(
+    diff_xf**2 + diff_pxf**2 + diff_yf**2 + diff_pyf**2 + diff_tf**2 + diff_ptf**2
+)
+norm_spin_diff = np.sqrt(diff_sxf**2 + diff_syf**2 + diff_szf**2)
+
+print("")
+print(":")
+print("Phase space vector norm:")
+print(norm_phase_space_vector)
+
+print("")
+print(":")
+print("Phase space difference norm:")
+print(norm_phase_space_diff)
+
+m, b = np.polyfit(np.log(norm_phase_space_vector), np.log(norm_phase_space_diff), deg=1)
+print(f"  slope={m}")
+
+# Test for  quadratic scaling with initial phase space vector:
+rtol = 2.0e-2
+print(f"  rtol={rtol}")
+assert np.allclose(
+    [m],
+    [
+        2.0,
+    ],
+    rtol=rtol,
+)
+
+print(":")
+print("Spin difference norm:")
+print(norm_spin_diff)
+
+m, b = np.polyfit(np.log(norm_phase_space_vector), np.log(norm_spin_diff), deg=1)
+print(f"  slope={m}")
+
+# Test for  quadratic scaling with initial phase space vector:
+rtol = 2.0e-2
+print(f"  rtol={rtol}")
+assert np.allclose(
+    [m],
+    [
+        2.0,
+    ],
+    rtol=rtol,
+)

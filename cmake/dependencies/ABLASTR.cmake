33c642fea57e90e881952ef80a6764a9ef697b32
Axel Huebl <axel.huebl@plasma.ninja>
2026-06-19 14:09:02 -0700

    AMReX development
    Co-authored-by: Axel Huebl <axel.huebl@plasma.ninja>

--- cmake/dependencies/ABLASTR.cmake
+++ cmake/dependencies/ABLASTR.cmake
@@ -183,10 +183,10 @@ set(ImpactX_ablastr_branch "759c5cb11a417187edc764b853cfb68018b91fce"
     "Repository branch for ImpactX_ablastr_repo if(ImpactX_ablastr_internal)")
 
 # AMReX is transitively pulled through ABLASTR
-set(ImpactX_amrex_repo "https://github.com/WeiqunZhang/amrex.git"
+set(ImpactX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(ImpactX_amrex_internal)")
-set(ImpactX_amrex_branch "fft_padding"
+set(ImpactX_amrex_branch "c5d27c8ff1b7b32238ebc75a9642a3fc36e65e9c"
     CACHE STRING
     "Repository branch for ImpactX_amrex_repo if(ImpactX_amrex_internal)")
 
